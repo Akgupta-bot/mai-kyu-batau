71a32b9913ced6a747e67849a868184f5cbf3c04
Drita-ai <alok.kr300@gmail.com>
2025-09-03 15:46:02 +0530

    Added better functionality in story mode

--- frontend/src/pages/StoryMode.jsx
+++ frontend/src/pages/StoryMode.jsx
@@ -1,19 +1,49 @@
 import BasketBallImg from '/basketball-court.jpg'
 import AnimatedCharacter from '/animated-character-unscreen.gif'
 import AnimatedCharacterFixed from '/animated-character-unscreen-fixed.png'
-import { useState } from 'react'
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
+import { useEffect, useState } from 'react'
+import QuestionCard from '../components/story-mode/QuestionCard'
+import client from '../../services/elevenLabs'
+import LyffyAudio from '/luffy.mp3'
+
+const SCRIPT = {
+    'displayText': 'Let us start the scenario',
+    'player1': 'Hey! how are you?',
+    'player2': 'I am fine, what about you?'
+}
 
 export default function StoryMode() {
     const [characterPlay, setCharacterPlay] = useState(false);
+    const [isGameStart, setIsGameStart] = useState(false);
 
+    // Function to Start the Module
     function handlePlay() {
-        console.log(characterPlay)
+        setIsGameStart(true)
+
+        // 1) Use the cached audio to generate sound using React Query
+    }
+
+    // Function to start Character Movement & Audio
+    function handleCharacterPlay() {
         setCharacterPlay((play) => !play)
     }
 
-    function handleStop() { }
+    useEffect(() => {
+        async function handleStop() {
+            await client.textToSpeech.convert("JBFqnCBsd6RMkjVDRZzb", {
+                outputFormat: "mp3_44100_128",
+                text: "The first move is what sets everything in motion.",
+                modelId: "eleven_multilingual_v2"
+            });
+        }
+
+        // 1) Fetch Storyline
+        // 2) Generate audio based on Storyline
+        // 3) Save it in state and use it ( React Query )
+
+        setCharacterPlay((play) => !play)
+    }, [])
+
     return (
         <div className="h-screen w-full overflow-hidden flex flex-col items-center justify-center space-y-6 p-4">
             {/* BASKETBALL SCENE */}
@@ -25,94 +55,63 @@ export default function StoryMode() {
                 />
 
                 <div className="absolute inset-0 bg-black/40"></div>
+                {!isGameStart && <div className="absolute inset-0 bg-black/40">
+                    <h1>Please Press Start...</h1>
+                </div>}
 
-                {/* Left Character */}
-                <div className="absolute left-5 top-1/2 -translate-y-1/2 flex flex-col items-center space-y-6">
-                    {!characterPlay && (
-                        <>
-                            <p className="text-2xl text-black bg-white/60 px-3 py-1 rounded-lg">
-                                Hey there
-                            </p>
-                            <img
-                                src={AnimatedCharacter}
-                                className="w-28 cursor-pointer"
-                                onClick={handlePlay}
-                            />
-                        </>
-                    )}
+                {isGameStart && <>{/* Left Character */}
+                    <div className="absolute left-5 top-1/2 -translate-y-1/2 flex flex-col items-center space-y-6">
+                        {!characterPlay && (
+                            <>
+                                <p className="text-2xl text-black bg-white/60 px-3 py-1 rounded-lg">
+                                    Hey there
+                                </p>
+                                <img
+                                    src={AnimatedCharacter}
+                                    className="w-125 cursor-pointer"
+                                    onClick={handlePlay}
+                                />
+                            </>
+                        )}
 
-                    {characterPlay && (
-                        <>
-                            <img
-                                src={AnimatedCharacterFixed}
-                                className="w-28 cursor-pointer"
-                                onClick={handlePlay}
-                            />
-                        </>
-                    )}
-                </div>
+                        {characterPlay && (
+                            <>
+                                <img
+                                    src={AnimatedCharacterFixed}
+                                    className="w-50 cursor-pointer"
+                                    onClick={handlePlay}
+                                />
+                            </>
+                        )}
+                    </div>
 
-                {/* Right Character */}
-                <div className="absolute right-5 top-1/2 -translate-y-1/2 flex flex-col items-center space-y-6">
-                    {characterPlay && (
-                        <>
-                            <p className="text-2xl text-black bg-white/60 px-3 py-1 rounded-lg">
-                                Hey, how have you been?
-                            </p>
-                            <img
-                                src={AnimatedCharacterFixed}
-                                className="w-28 cursor-pointer"
-                                onClick={handlePlay}
-                            />
-                        </>
-                    )}
+                    {/* Right Character */}
+                    <div className="absolute right-5 top-1/2 -translate-y-1/2 flex flex-col items-center space-y-6">
+                        {characterPlay && (
+                            <>
+                                <p className="text-2xl text-black bg-white/60 px-3 py-1 rounded-lg">
+                                    Hey, how have you been?
+                                </p>
+                                <img
+                                    src={AnimatedCharacter}
+                                    className="w-125 cursor-pointer"
+                                    onClick={handlePlay}
+                                />
+                            </>
+                        )}
 
-                    {!characterPlay && (
-                        <>
-                            <img
-                                src={AnimatedCharacter}
-                                className="w-28 cursor-pointer"
-                                onClick={handlePlay}
-                            />
-                        </>
-                    )}
-                </div>
+                        {!characterPlay && (
+                            <>
+                                <img
+                                    src={AnimatedCharacterFixed}
+                                    className="w-50 cursor-pointer"
+                                    onClick={handlePlay}
+                                />
+                            </>
+                        )}
+                    </div></>}
             </div>
-
-            <Card className="w-[60%] h-[30%] shadow-2xl rounded-2xl border border-gray-200 bg-gradient-to-br from-white to-gray-200">
-                <CardHeader>
-                    <CardTitle className="text-xl font-semibold text-gray-800 text-center">
-                        What’s your favorite sport?
-                    </CardTitle>
-                </CardHeader>
-
-                <CardContent className="grid grid-cols-2 gap-4">
-                    <Button
-                        variant="outline"
-                        className="h-14 text-base font-medium rounded-xl transition-all hover:scale-105 hover:border-blue-500 hover:text-blue-600"
-                    >
-                        Basketball
-                    </Button>
-                    <Button
-                        variant="outline"
-                        className="h-14 text-base font-medium rounded-xl transition-all hover:scale-105 hover:border-green-500 hover:text-green-600"
-                    >
-                        Football
-                    </Button>
-                    <Button
-                        variant="outline"
-                        className="h-14 text-base font-medium rounded-xl transition-all hover:scale-105 hover:border-red-500 hover:text-red-600"
-                    >
-                        Cricket
-                    </Button>
-                    <Button
-                        variant="outline"
-                        className="h-14 text-base font-medium rounded-xl transition-all hover:scale-105 hover:border-purple-500 hover:text-purple-600"
-                    >
-                        Tennis
-                    </Button>
-                </CardContent>
-            </Card>
+            <QuestionCard isGameStart={isGameStart} onHandlePlay={handlePlay} />
         </div>
     )
 }
