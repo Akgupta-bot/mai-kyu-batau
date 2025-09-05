093abaf3a663669429d2303d4e8cd888c954f648
Drita-ai <alok.kr300@gmail.com>
2025-09-05 22:33:00 +0530

    Added scenarios titles

--- frontend/src/components/story-mode/ToggleScene.jsx
+++ frontend/src/components/story-mode/ToggleScene.jsx
@@ -2,16 +2,19 @@ export default function ToggleScene() {
     return (
         <div className="w-[25%] flex flex-col space-y-4 absolute right-[-220px] border border-white p-2 rounded-2xl">
             <div className="flex-1 rounded-xl overflow-hidden relative bg-[#424242] flex items-center justify-center p-2">
-                <span className="text-white">Theme 1</span>
+                <span className="text-white">Beginnings</span>
             </div>
             <div className="flex-1 rounded-xl overflow-hidden relative bg-[#424242] flex items-center justify-center p-2">
-                <span className="text-white">Theme 2</span>
+                <span className="text-white">Temptations</span>
             </div>
             <div className="flex-1 rounded-xl overflow-hidden relative bg-[#424242] flex items-center justify-center p-2">
-                <span className="text-white">Theme 3</span>
+                <span className="text-white">Consequences</span>
             </div>
             <div className="flex-1 rounded-xl overflow-hidden relative bg-[#424242] flex items-center justify-center p-2">
-                <span className="text-white">Theme 4</span>
+                <span className="text-white">Redemption</span>
+            </div>
+            <div className="flex-1 rounded-xl overflow-hidden relative bg-[#424242] flex items-center justify-center p-2">
+                <span className="text-white">Legacy</span>
             </div>
         </div>)
 }
